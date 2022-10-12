57e7a1630fe4ea859a301c0c40778f5b973f2f83
Gabriel Diaz <8962011+gabrielDiaz-performlab@users.noreply.github.com>
2022-10-12 09:51:20 -0400

    v 0.001

--- player_settings/plugins/gazerHMD_1.py
+++ player_settings/plugins/gazerHMD_1.py
@@ -9,9 +9,9 @@ from gaze_mapping.notifications import (
     CalibrationResultNotification,
 )
 import file_methods as fm
-import player_methods as pm
 
-import cv2
+import av
+import numpy as np
 
 CUSTOM_TOPIC = "custom_topic"
 
@@ -38,13 +38,18 @@ class load_3d_references(Plugin):
         self.start_time, self.end_time = self._set_time_range_from_trim()
         self.pupilDatum_conf_threshold = 0.8
 
+        self.pupil_0_2d_data = []
+        self.pupil_1_2d_data = []
+
+        self.pupil_0_3d_data = []
+        self.pupil_1_3d_data = []
+
         self.eye0_detector3d = Pye3DPlugin(g_pool=g_pool).pupil_detector
         self.eye1_detector3d = Pye3DPlugin(g_pool=g_pool).pupil_detector
 
         self._stop_other_pupil_detectors()
         self._load_2D_pupil_datum()
-        # self._produce_3D_pupil_data()
-
+        self._produce_3D_pupil_data()
 
         # initialize empty menu
         self.menu = None
@@ -76,12 +81,12 @@ class load_3d_references(Plugin):
         for loaded_datum in pupil_data:
 
             if loaded_datum["topic"]=='pupil.0.2d' and loaded_datum["timestamp"] >= self.start_time and loaded_datum["timestamp"] <= self.end_time and loaded_datum["confidence"] >= self.pupilDatum_conf_threshold:
-
+                self.pupil_0_2d_data.append(loaded_datum)
                 observation = self.eye0_detector3d._extract_observation(loaded_datum)
                 self.eye0_detector3d.update_models(observation)
 
             elif loaded_datum["topic"] == 'pupil.1.2d' and loaded_datum["timestamp"] >= self.start_time and loaded_datum[ "timestamp"] <= self.end_time and loaded_datum["confidence"] >= self.pupilDatum_conf_threshold:
-
+                self.pupil_1_2d_data.append(loaded_datum)
                 observation = self.eye0_detector3d._extract_observation(loaded_datum)
                 self.eye1_detector3d.update_models(observation)
 
@@ -93,66 +98,38 @@ class load_3d_references(Plugin):
 
     def _produce_3D_pupil_data(self):
 
-
-        pupil_data = self.g_pool.pupil_positions
-        datum_list_3d = []
-        #
-        eye0VidCap = cv2.VideoCapture(self.g_pool.rec_dir  + 'eye0.mp4')
+        # eye0VidCap = cv2.VideoCapture(self.g_pool.rec_dir  + 'eye0.mp4')
         # eye1VidCap = cv2.VideoCapture(self.g_pool.rec_dir)
 
-        for datum_2D in pupil_data:
+        eye0_cont = av.open(self.g_pool.rec_dir + '\\eye0.mp4')
+        stream = eye0_cont.streams.video[0]
 
-            success, frame = eye0VidCap.read()
-            height, width = frame[:, :, 0].shape
+        for datum_2D, frame in zip(self.pupil_0_2d_data, eye0_cont.decode(video=0)):
 
-            frame = frame.copy(order="C")  # datum_list["debug_imgs"][idx]
+            frame_bgr = frame.reformat(frame.width, frame.height, 'bgr24')
+            frame_gray = frame.reformat(frame.width, frame.height, 'gray')
 
-            bgr = frame[:, :, 0]
-            bgr = bgr.copy(order="C")
-            gray = frame[:, :, 0]
-            gray = gray.copy(order="C")
+            bgr = frame_bgr.to_nd_array()
+            gray = frame_gray.to_nd_array()
             height, width = gray.shape
 
+            timestamp = datum_2D['timestamp']
 
-            if '2d' in datum_2D["topic"]:
+            # for count, timestamp in enumerate(self.g_pool.timestamps):
+            pupil_frame = lambda: None
+            setattr(pupil_frame, "gray", gray)
+            setattr(pupil_frame, "bgr", bgr)
+            setattr(pupil_frame, "width", width)
+            setattr(pupil_frame, "height", height)
+            setattr(pupil_frame, "timestamp", timestamp)
 
-                timestamp = datum_2D['timestamp']
+            pupil3d_datum = self.eye0_detector3d.detect(
+                pupil_frame, **{"previous_detection_results": [datum_2D]}
+            )
 
-                # for count, timestamp in enumerate(self.g_pool.timestamps):
-
-                pupil_frame = lambda: None
-                setattr(pupil_frame, "gray", gray)
-                setattr(pupil_frame, "bgr", bgr)
-                setattr(pupil_frame, "width", width)
-                setattr(pupil_frame, "height", height)
-                setattr(pupil_frame, "timestamp", timestamp)
-
-                if datum_2D["topic"] == 'pupil.0.2d':
-
-                    pupil3d_datum = self.eye0_detector3d.detect(
-                        pupil_frame, **{"previous_detection_results": [datum_2D]}
-                    )
-
-                    datum_list_3d.append(fm.Serialized_Dict(python_dict=pupil3d_datum))
-
-                elif datum_2D["topic"] == 'pupil.1.2d':
-
-                    pupil3d_datum = self.eye1_detector3d.detect(
-                        pupil_frame, **{"previous_detection_results": [datum_2D]}
-                    )
-
-                    datum_list_3d.append(fm.Serialized_Dict(python_dict=pupil3d_datum))
-
-        a=1;
-            # elif loaded_datum["topic"] == 'pupil.1.2d' and loaded_datum["timestamp"] >= self.start_time and \
-            #         loaded_datum["timestamp"] <= self.end_time and loaded_datum[
-            #     "confidence"] >= self.pupilDatum_conf_threshold:
-            #
-            #     pass
+            self.pupil_0_3d_data.append(fm.Serialized_Dict(python_dict=pupil3d_datum))
 
 
-
-        datum_list["3d"].append(fm.Serialized_Dict(python_dict=pupil3d_datum))
 
     def init_ui(self):
         self.add_menu()
